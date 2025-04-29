e02e63beb077952256bd0e2c502dadaa99e8a68f
vega2k8s <vega2k8s@gmail.com>
2025-04-29 17:17:32 +0900

    WebApplicationType update

--- src/main/java/com/basic/myspringboot/MySpringBootAppApplication.java
+++ src/main/java/com/basic/myspringboot/MySpringBootAppApplication.java
@@ -1,13 +1,23 @@
 package com.basic.myspringboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MySpringBootAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MySpringBootAppApplication.class, args);
+		//SpringApplication.run(MySpringBootAppApplication.class, args);
+		SpringApplication application = new SpringApplication(MySpringBootAppApplication.class);
+		application.setWebApplicationType(WebApplicationType.NONE);
+		application.run(args);
+	}
+
+	@Bean
+	public String hello() {
+		return "hello springboot";
 	}
 
 }
