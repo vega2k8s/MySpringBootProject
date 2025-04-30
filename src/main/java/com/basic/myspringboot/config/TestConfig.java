3d2209403af355c908fc555424d43db16ef74ed7
vega2k8s <vega2k8s@gmail.com>
2025-04-30 14:36:34 +0900

    TestConfig add

--- src/main/java/com/basic/myspringboot/config/TestConfig.java
+++ src/main/java/com/basic/myspringboot/config/TestConfig.java
@@ -7,6 +7,10 @@ import org.springframework.context.annotation.Configuration;
 public class TestConfig {
     @Bean
     public CustomerVO customerVO() {
+        return CustomerVO.builder()  //CustomerVOBuilder
+                .mode("테스트환경")
+                .rate(0.5)
+                .build();
 
     }
 }
