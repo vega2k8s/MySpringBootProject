d513ee3d578ca373dde38ade8a6c2e0ab0b9a051
vega2k8s <vega2k8s@gmail.com>
2025-04-30 14:43:16 +0900

    spring.profiles.active add

--- src/main/java/com/basic/myspringboot/config/ProdConfig.java
+++ src/main/java/com/basic/myspringboot/config/ProdConfig.java
@@ -2,8 +2,10 @@ package com.basic.myspringboot.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("prod")
 public class ProdConfig {
     @Bean
     public CustomerVO customerVO() {
--- src/main/java/com/basic/myspringboot/config/TestConfig.java
+++ src/main/java/com/basic/myspringboot/config/TestConfig.java
@@ -2,8 +2,10 @@ package com.basic.myspringboot.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("test")
 public class TestConfig {
     @Bean
     public CustomerVO customerVO() {
