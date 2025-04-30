6c5e4bef3bbcc5dc879d6f41be0106e6fdfce6e1
vega2k8s <vega2k8s@gmail.com>
2025-04-30 16:09:29 +0900

    Logger ìformat update

--- src/main/java/com/basic/myspringboot/runner/MyRunner.java
+++ src/main/java/com/basic/myspringboot/runner/MyRunner.java
@@ -33,21 +33,21 @@ public class MyRunner implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
 
-        System.out.println("${myboot.name} = " + name);
-        System.out.println("${myboot.age} = " + age);
-        System.out.println("${myboot.fullName} = " + environment.getProperty("myboot.fullName"));
+        logger.debug("${myboot.name} = {}", name);
+        logger.debug("${myboot.age} = {}", age);
+        logger.debug("${myboot.fullName} = {}", environment.getProperty("myboot.fullName"));
 
-        System.out.println("MyBootProperties getName() " + properties.getName());
-        System.out.println("MyBootProperties getAge() " + properties.getAge());
-        System.out.println("MyBootProperties getFullName() " + properties.getFullName());
-        System.out.println("설정된 Port 번호 " + environment.getProperty("local.server.port") );
+        logger.info("MyBootProperties getName() = {}", properties.getName());
+        logger.info("MyBootProperties getAge() = {}", properties.getAge());
+        logger.info("MyBootProperties getFullName() = {}", properties.getFullName());
+        logger.info("설정된 Port 번호 = {}", environment.getProperty("local.server.port") );
 
-        System.out.println("현재 활성화된 CustomerVO Bean " + customerVO);
+        logger.info("현재 활성화된 CustomerVO Bean = {}", customerVO);
 
         // foo 라는 VM 아규먼트 있는지 확인
-        System.out.println("VM 아규먼트 foo : " + args.containsOption("foo"));
+        logger.debug("VM 아규먼트 foo : {}", args.containsOption("foo"));
         // bar 라는 Program 아규먼트 있는지 확인
-        System.out.println("Program 아규먼트 bar : " + args.containsOption("bar"));
+        logger.debug("Program 아규먼트 bar : {}", args.containsOption("bar"));
 
         /*
             Iterable forEach(Consumer)
