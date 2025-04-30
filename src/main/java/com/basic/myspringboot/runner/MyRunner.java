8b94783a2a33c8a81d2be9908a3bc86165dde3d5
vega2k8s <vega2k8s@gmail.com>
2025-04-30 11:26:33 +0900

    props add

--- src/main/java/com/basic/myspringboot/runner/MyRunner.java
+++ src/main/java/com/basic/myspringboot/runner/MyRunner.java
@@ -1,13 +1,23 @@
 package com.basic.myspringboot.runner;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyRunner implements ApplicationRunner {
+    @Value("${myboot.name}")
+    private String name;
+
+    @Value("${myboot.age}")
+    private int age;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        System.out.println("${myboot.name} = " + name);
+        System.out.println("${myboot.age} = " + age);
+
         // foo 라는 VM 아규먼트 있는지 확인
         System.out.println("VM 아규먼트 foo : " + args.containsOption("foo"));
         // bar 라는 Program 아규먼트 있는지 확인
@@ -18,7 +28,7 @@ public class MyRunner implements ApplicationRunner {
             Consumer 는 함수형 인터페이스 void accept(T t)
             Consumer 의 추상메서드를 오버라이딩 하는 구문을 람다식으로 작성
          */
-        //아규먼트 목록 출력
+        // Program 아규먼트 목록 출력
         args.getOptionNames()  //Set<String>
                 .forEach(name -> System.out.println(name));
 
