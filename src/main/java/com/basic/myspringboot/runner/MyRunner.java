32168a4b1a064e921cc661981bf6f915bba97beb
vega2k8s <vega2k8s@gmail.com>
2025-04-30 11:03:04 +0900

    MyRunner ㅕ update

--- src/main/java/com/basic/myspringboot/runner/MyRunner.java
+++ src/main/java/com/basic/myspringboot/runner/MyRunner.java
@@ -8,7 +8,19 @@ import org.springframework.stereotype.Component;
 public class MyRunner implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        // foo 라는 VM 아규먼트 있는지 확인
         System.out.println("VM 아규먼트 foo : " + args.containsOption("foo"));
+        // bar 라는 Program 아규먼트 있는지 확인
         System.out.println("Program 아규먼트 bar : " + args.containsOption("bar"));
-    }
-}
+
+        /*
+            Iterable forEach(Consumer)
+            Consumer 는 함수형 인터페이스 void accept(T t)
+            Consumer 의 추상메서드를 오버라이딩 하는 구문을 람다식으로 작성
+         */
+        //아규먼트 목록 출력
+        args.getOptionNames()  //Set<String>
+                .forEach(name -> System.out.println(name));
+
+    }//run
+}//class
