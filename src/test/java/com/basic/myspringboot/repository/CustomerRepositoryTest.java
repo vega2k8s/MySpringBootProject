19f0e88048a144cebf755dc89962334727d583a2
vega2k8s <vega2k8s@gmail.com>
2025-05-07 15:26:40 +0900

    Customer test update

--- src/test/java/com/basic/myspringboot/repository/CustomerRepositoryTest.java
+++ src/test/java/com/basic/myspringboot/repository/CustomerRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -19,6 +20,9 @@ class CustomerRepositoryTest {
         customer.setCustomerId("A001");
         customer.setCustomerName("스프링");
         //When (실행 단계)
+        Customer addCustomer = customerRepository.save(customer);
         //Then (검증 단계)
+        assertThat(addCustomer).isNotNull();
+        assertThat(addCustomer.getCustomerName()).isEqualTo("스프링");
     }
 }
