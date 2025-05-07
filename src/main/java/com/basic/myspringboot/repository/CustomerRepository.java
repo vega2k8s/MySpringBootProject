387cb57d6bac8a94d9b7c6122c152f6866081df0
vega2k8s <vega2k8s@gmail.com>
2025-05-07 14:17:31 +0900

    CustomerRepository query method add

--- src/main/java/com/basic/myspringboot/repository/CustomerRepository.java
+++ src/main/java/com/basic/myspringboot/repository/CustomerRepository.java
@@ -3,9 +3,11 @@ package com.basic.myspringboot.repository;
 import com.basic.myspringboot.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CustomerRepository extends JpaRepository<Customer,Long> {
-    //Query Method 정의
-    Optional<Customer> findByCustomerId(String customerId);
+    //Query Method 정의하면 JPA가 JPQL(java persistence query language)
+    Optional<Customer> findByCustomerId(String id);
+    List<Customer> findByCustomerName(String name);
 }
