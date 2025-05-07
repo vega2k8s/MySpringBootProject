30d20704544a50fe9b4aa049273ea76217fd6779
vega2k8s <vega2k8s@gmail.com>
2025-05-07 14:12:28 +0900

    CustomerRepository query method add

--- src/main/java/com/basic/myspringboot/repository/CustomerRepository.java
+++ src/main/java/com/basic/myspringboot/repository/CustomerRepository.java
@@ -3,5 +3,9 @@ package com.basic.myspringboot.repository;
 import com.basic.myspringboot.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CustomerRepository extends JpaRepository<Customer,Long> {
+    //Query Method 정의
+    Optional<Customer> findByCustomerId(String customerId);
 }
