e3ba91fad332b2c452d5aab3d25357567d47dbc5
vega2k8s <vega2k8s@gmail.com>
2025-05-07 15:47:06 +0900

    Customer test @Rollback false

--- src/test/java/com/basic/myspringboot/repository/CustomerRepositoryTest.java
+++ src/test/java/com/basic/myspringboot/repository/CustomerRepositoryTest.java
@@ -4,25 +4,29 @@ import com.basic.myspringboot.entity.Customer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
 class CustomerRepositoryTest {
     @Autowired
     CustomerRepository customerRepository;
 
     @Test
+    @Rollback(value = false) //Rollback 처리하지 마세요!!
     void testCreateCustomer() {
         //Given (준비 단계)
         Customer customer = new Customer();
-        customer.setCustomerId("A001");
-        customer.setCustomerName("스프링");
+        customer.setCustomerId("A002");
+        customer.setCustomerName("스프링2");
         //When (실행 단계)
         Customer addCustomer = customerRepository.save(customer);
         //Then (검증 단계)
         assertThat(addCustomer).isNotNull();
-        assertThat(addCustomer.getCustomerName()).isEqualTo("스프링");
+        assertThat(addCustomer.getCustomerName()).isEqualTo("스프링2");
     }
 }
