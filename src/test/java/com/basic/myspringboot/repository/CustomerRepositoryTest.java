539ec23d8ee687c6ca811299ec9015d99dae10db
vega2k8s <vega2k8s@gmail.com>
2025-05-07 16:06:20 +0900

    CustomerRepositoryTest add

--- src/test/java/com/basic/myspringboot/repository/CustomerRepositoryTest.java
+++ src/test/java/com/basic/myspringboot/repository/CustomerRepositoryTest.java
@@ -1,23 +1,35 @@
 package com.basic.myspringboot.repository;
 
 import com.basic.myspringboot.entity.Customer;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
+//@DataJpaTest
 class CustomerRepositoryTest {
     @Autowired
     CustomerRepository customerRepository;
 
     @Test
+    void testFindBy() {
+        Optional<Customer> optionalCustomer = customerRepository.findById(1L);
+        assertThat(optionalCustomer).isNotEmpty();
+    }
+
+    @Test
     @Rollback(value = false) //Rollback 처리하지 마세요!!
+    @Disabled
     void testCreateCustomer() {
         //Given (준비 단계)
         Customer customer = new Customer();
@@ -29,4 +41,6 @@ class CustomerRepositoryTest {
         assertThat(addCustomer).isNotNull();
         assertThat(addCustomer.getCustomerName()).isEqualTo("스프링2");
     }
+
+
 }
