1b2a85b82f85e46a56252694c0371976e22a5e6c
vega2k8s <vega2k8s@gmail.com>
2025-05-08 10:44:27 +0900

    user controller update

--- src/main/java/com/basic/myspringboot/controller/UserRestController.java
+++ src/main/java/com/basic/myspringboot/controller/UserRestController.java
@@ -1,15 +1,24 @@
 package com.basic.myspringboot.controller;
 
 import com.basic.myspringboot.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 //@Controller + @ResponseBody
 @RestController
+@RequiredArgsConstructor
+//final 인 변수를 초기화하는 생성자를 자동으로 생성해주는 역할을 하는 롬복 어노테이션
+@RequestMapping("/api/users")
 public class UserRestController {
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
-    public UserRestController(UserRepository userRepository) {
-        System.out.println(">>> UserController " + userRepository.getClass().getName());
-        this.userRepository = userRepository;
-    }
+    //Constructor Injection
+//    public UserRestController(UserRepository userRepository) {
+//        System.out.println(">>> UserController " + userRepository.getClass().getName());
+//        this.userRepository = userRepository;
+//    }
+
+
+
 }
