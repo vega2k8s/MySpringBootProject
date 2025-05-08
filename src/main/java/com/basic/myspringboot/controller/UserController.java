57cca6333247c8dbaf3b2bd63f18cd5daf6d49a7
vega2k8s <vega2k8s@gmail.com>
2025-05-08 17:14:44 +0900

    usercontroller index method add

--- src/main/java/com/basic/myspringboot/controller/UserController.java
+++ src/main/java/com/basic/myspringboot/controller/UserController.java
@@ -1,11 +1,22 @@
 package com.basic.myspringboot.controller;
 
+import com.basic.myspringboot.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/index")
+    public String index(Model model) {
+        model.addAttribute("users", userRepository.findAll());
+        return "index";
+    }
+
     @GetMapping("/thymeleaf")
     public String leaf(Model model) {
         model.addAttribute("name", "스프링부트!");
