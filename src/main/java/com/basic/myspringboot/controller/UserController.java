036c8c5e20a804efda7f968ed3e2bde61a51c970
vega2k8s <vega2k8s@gmail.com>
2025-05-09 11:10:01 +0900

    fields.errors

--- src/main/java/com/basic/myspringboot/controller/UserController.java
+++ src/main/java/com/basic/myspringboot/controller/UserController.java
@@ -6,7 +6,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,7 +29,7 @@ public class UserController {
 
     @PostMapping("/adduser")
     public String addUser(@Valid @ModelAttribute("userForm") User user,
-                          BindingResult result, Model model) {
+                          Errors result, Model model) {
         if (result.hasErrors()) {
             return "add-user";
         }
