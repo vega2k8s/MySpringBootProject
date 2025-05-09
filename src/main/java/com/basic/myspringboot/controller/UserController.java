d57a9262459857a1eb3796318b960093afa15102
vega2k8s <vega2k8s@gmail.com>
2025-05-09 10:42:21 +0900

    controller addUser add

--- src/main/java/com/basic/myspringboot/controller/UserController.java
+++ src/main/java/com/basic/myspringboot/controller/UserController.java
@@ -2,11 +2,14 @@ package com.basic.myspringboot.controller;
 
 import com.basic.myspringboot.entity.User;
 import com.basic.myspringboot.repository.UserRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
@@ -20,8 +23,20 @@ public class UserController {
     }
 
     @GetMapping("/signup")
-    public String showSignUpForm(@ModelAttribute("user") User user) {
+    public String showSignUpForm(@ModelAttribute("userForm") User user) {
         return "add-user";
+    }
+
+    @PostMapping("/adduser")
+    public String addUser(@Valid @ModelAttribute("userForm") User user,
+                          BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "add-user";
+        }
+        userRepository.save(user);
+        model.addAttribute("users", userRepository.findAll());
+        return "index";
+        //return "redirect:/index";
     }
 
     @GetMapping("/thymeleaf")
