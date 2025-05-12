10eb93ef25b6068c3e63c3b19a3109744a23255e
vega2k8s <vega2k8s@gmail.com>
2025-05-12 17:00:32 +0900

    relation add

---
+++ src/main/java/com/basic/myspringboot/entity/Student.java
@@ -0,0 +1,31 @@
+package com.basic.myspringboot.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+//Student
+@Entity
+@Table(name = "students")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter @Setter
+public class Student {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "student_id")
+    private Long id;
+    
+    @Column(nullable = false)
+    private String name;
+    
+    @Column(unique = true, nullable = false)
+    private String studentNumber;
+
+    //1:1 지연로딩
+    //양방향 Student에서 StudentDetail 참조할 수 있도록 FK에 해당하는 필드명 mappedBy에 설정한다.
+    //Student와 StudentDetail 의 라이프사이클이 동일하다.
+    @OneToOne(fetch = FetchType.LAZY, mappedBy = "student", cascade = CascadeType.ALL)
+    private StudentDetail studentDetail;
+}
