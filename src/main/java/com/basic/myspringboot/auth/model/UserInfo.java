8539f817c406ad47aafdd6df69ec36c18aaca116
vega2k8s <vega2k8s@gmail.com>
2025-05-12 14:19:06 +0900

    UserInfoUserDetails add

--- src/main/java/com/basic/myspringboot/auth/model/UserInfo.java
+++ src/main/java/com/basic/myspringboot/auth/model/UserInfo.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 public class UserInfo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Integer id;
 
     @Column(nullable = false)
     private String name;
