520a3c71ae1c011592c0194a0c5d63002088ba54
mysoyul <vega2k_@naver.com>
2025-09-03 19:49:48 +0900

    Paged Controller

--- src/main/java/com/basic/myspringboot/repository/StudentRepository.java
+++ src/main/java/com/basic/myspringboot/repository/StudentRepository.java
@@ -12,7 +12,6 @@ import java.util.List;
 import java.util.Optional;
 
 //StudentRepository 인터페이스
-@Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
     Optional<Student> findByStudentNumber(String studentNumber);
 
