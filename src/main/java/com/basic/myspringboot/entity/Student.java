064bcc115ac2406608722786663bf087d17452c3
vega2k8s <vega2k8s@gmail.com>
2026-09-03 22:03:55 +0900

    student 단독

--- src/main/java/com/basic/myspringboot/entity/Student.java
+++ src/main/java/com/basic/myspringboot/entity/Student.java
@@ -10,7 +10,6 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 @Getter @Setter
-//Non-Owner(종속) 양방향관계
 public class Student {
     
     @Id
@@ -23,10 +22,4 @@ public class Student {
     
     @Column(unique = true, nullable = false)
     private String studentNumber;
-
-    //1:1 지연로딩
-    //양방향 Student에서 StudentDetail 참조할 수 있도록 FK에 해당하는 필드명 mappedBy에 설정한다.
-    //Student와 StudentDetail 의 라이프사이클이 동일하다.
-    @OneToOne(fetch = FetchType.LAZY, mappedBy = "student", cascade = CascadeType.ALL)
-    private StudentDetail studentDetail;
 }
