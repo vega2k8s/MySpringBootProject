055be2a5cc028e0530aa007f65cab642eedda101
vega2k8s <vega2k8s@gmail.com>
2026-09-09 18:27:02 +0900

    runner update

--- src/main/java/com/basic/myspringboot/runner/DataInitRunner.java
+++ src/main/java/com/basic/myspringboot/runner/DataInitRunner.java
@@ -136,23 +136,7 @@ public class DataInitRunner implements CommandLineRunner {
 //                .studentNumber("CS003")
 //                .department(cs)
 //                .build();
-
-        //email과 phonenumber 만 가진 StuentDetail 객체생성하기
-        StudentDetail detail8 = StudentDetail.builder()
-                .phoneNumber("010-6789-0789")
-                .email("helen@example.com")
-                .build();
-
-        // Student without detail (Computer Science)
-        Student student8 = Student.builder()
-                .name("Helen Lee")
-                .studentNumber("CS003")
-                .studentDetail(detail8)
-                .department(cs)
-                .build();
-
-        detail8.setStudent(student8);
-
+        z
         List<Student> students = studentRepository.saveAll(
                 List.of(student1, student2, student3, student4, student5, student6, student7, student8)
         );
